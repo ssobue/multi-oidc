afd5dc3cf5a55007783cd4822dadd343b48209e5
SOBUE Sho <sho@sobue.dev>
2020-11-17 20:54:26 +0900

    Modify Tests

--- src/test/java/dev/sobue/oidc/MultiOidcApplicationTests.java
+++ src/test/java/dev/sobue/oidc/MultiOidcApplicationTests.java
@@ -1,13 +1,13 @@
 package dev.sobue.oidc;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class MultiOidcApplicationTests {
 
   @Test
   void contextLoads() {
+    assertDoesNotThrow(() -> MultiOidcApplication.main(new String[0]));
   }
-
 }
