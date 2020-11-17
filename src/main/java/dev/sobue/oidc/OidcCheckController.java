90322f05ff9ad3fc2a432f2c94fd9e2734120aa5
SOBUE Sho <sho@sobue.dev>
2020-11-17 22:19:24 +0900

    Add OIDC Provider. Google, GitHub, Yahoo! JAPAN

---
+++ src/main/java/dev/sobue/oidc/OidcCheckController.java
@@ -0,0 +1,13 @@
+package dev.sobue.oidc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OidcCheckController {
+
+  @GetMapping
+  public String index() {
+    return "top";
+  }
+}
