acaaa2da250f24b41abbaad328c2016c70775089
SOBUE Sho <sho@sobue.dev>
2020-11-17 20:38:44 +0900

    Spring Boot 2.4.0 Application

---
+++ src/main/java/dev/sobue/oidc/MultiOidcApplication.java
@@ -0,0 +1,13 @@
+package dev.sobue.oidc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class MultiOidcApplication {
+
+  public static void main(String[] args) {
+    run(MultiOidcApplication.class, args);
+  }
+}
---
+++ src/test/java/dev/sobue/oidc/MultiOidcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sobue.oidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiOidcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
